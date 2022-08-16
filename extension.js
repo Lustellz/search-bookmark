98a0facfb5abf993ac7343c7b8d348ab8cd00687
권령경 <SGO1017606@mobmail.shinhan.com>
2022-08-16 18:06:37 +0900

    fix command

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,25 +12,25 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-bookmark" is now active!');
+	console.log('Congratulations, your extension "searchBookmark" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('search-bookmark.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('searchBookmark.startMarking', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from search-bookmark!');
+		vscode.window.showInformationMessage('Your book-mark is now available.');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
-module.exports = {
+export default {
 	activate,
 	deactivate
 }
